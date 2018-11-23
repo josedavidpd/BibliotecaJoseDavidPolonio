7389399057c32e03d5d7b37c556fe575a471ee24
jdpolonio <you@example.com>
2018-11-23 12:45:12 +0100

    Ruta admin correctamente

--- CofTrianaApp/src/app/dashboard/dashboard.routing.ts
+++ CofTrianaApp/src/app/dashboard/dashboard.routing.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 
 import { DashboardComponent } from './dashboard.component';
+import { AdminComponent } from './admin/admin.component';
 
-export const DashboardRoutes: Routes = [{
-  path: 'dashboard',
-  component: DashboardComponent
-}];
+export const DashboardRoutes: Routes = [
+  {path: 'dashboard',component: DashboardComponent},
+  {path: 'admin',component: AdminComponent}
+
+
+
+];
