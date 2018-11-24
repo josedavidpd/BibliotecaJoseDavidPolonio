95d91407c3bc00286855aeee359071e15a189390
José David <38279195+josedavidpd@users.noreply.github.com>
2018-11-24 15:53:20 +0100

    Creado servicio para añadir nuevo recurso

---
+++ CofTrianaApp/src/app/dashboard/interfaces/add-recurso.interface.ts
@@ -0,0 +1,8 @@
+export interface AddRecurso {
+  title: string;
+  autor: string;
+  anuy: string;
+  content: string;
+  typeId: number;
+  categoryId: number;
+}
--- CofTrianaApp/src/app/dashboard/services/recursos.service.ts
+++ CofTrianaApp/src/app/dashboard/services/recursos.service.ts
@@ -3,6 +3,7 @@ import { SessionService } from './../../session/services/session.service';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { AddRecurso } from '../interfaces/add-recurso.interface';
 
 
 const recursoUrl = `${environment.apiUrl}/recurso`;
@@ -39,7 +40,22 @@ export class RecursosService {
       })
     };
 
-    return this.http.delete(`${recursoUrl}/${id}`,requestOptions)
+    return this.http.delete(`${recursoUrl}/${id}`,requestOptions);
+
+  }
+
+
+  addRecurso(nuevoRecurso: AddRecurso){
+
+    const requestOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${this.sessionService.getToken()}`,
+        'Access-Control-Allow-Origin': '*'
+      })
+    };
+
+    return this.http.post<AddRecurso>(`${recursoUrl}/create`,nuevoRecurso,requestOptions);
 
   }
 }
