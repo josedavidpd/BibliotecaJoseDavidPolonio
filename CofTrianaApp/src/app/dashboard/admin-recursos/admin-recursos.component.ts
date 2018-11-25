91c8d8fb2dbdf56660d7ae8c08d49f0973e939fb
José David <38279195+josedavidpd@users.noreply.github.com>
2018-11-25 14:36:35 +0100

    Filtrar en 'admin-recursos' hecho

--- CofTrianaApp/src/app/dashboard/admin-recursos/admin-recursos.component.ts
+++ CofTrianaApp/src/app/dashboard/admin-recursos/admin-recursos.component.ts
@@ -1,58 +1,74 @@
+import { Component, OnInit } from "@angular/core";
+import { RecursosService } from "../services/recursos.service";
+import { Recursos } from "../interfaces/recursos.interface";
+import { MatDialog, MatTableDataSource } from "@angular/material";
+import { AddRecursoComponent } from "../add-recurso/add-recurso.component";
 
-import { Component, OnInit } from '@angular/core';
-import { RecursosService } from '../services/recursos.service';
-import { Recursos } from '../interfaces/recursos.interface';
-import { MatDialog } from '@angular/material';
-import { AddRecursoComponent } from '../add-recurso/add-recurso.component';
-
+const ELEMENT_DATA: Recursos[] = [];
 
 @Component({
-  selector: 'app-admin-recursos',
-  templateUrl: './admin-recursos.component.html',
-  styleUrls: ['./admin-recursos.component.css']
+  selector: "app-admin-recursos",
+  templateUrl: "./admin-recursos.component.html",
+  styleUrls: ["./admin-recursos.component.css"]
 })
 export class AdminRecursosComponent implements OnInit {
-
-
-     constructor(private recursoService: RecursosService, public dialog: MatDialog ) { }
+  constructor(
+    private recursoService: RecursosService,
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit() {
     this.getRecursos();
   }
 
-  displayedColumns: string[] = ['Id', 'Nombre', 'Tipo', 'Categoria', 'Autor', 'Fecha de publicacion', 'Acciones'];
-  dataSource : Recursos[];
+  displayedColumns: string[] = [
+    "Id",
+    "Nombre",
+    "Tipo",
+    "Categoria",
+    "Autor",
+    "Fecha de publicacion",
+    "Acciones"
+  ];
 
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
 
-  getRecursos(){
-    this.recursoService.getAllRecursos().subscribe(recursos => {
-      this.dataSource = recursos;
-    }, error => {
-      console.log(error);
-    }
-    )
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
-  borrarRecurso(id: number){
-    this.recursoService.deleteRecurso(id).subscribe(response => {
-      this.getRecursos();
-    }, error => {
-      console.log(error);
-    }
-    )
+  getRecursos() {
+    this.recursoService.getAllRecursos().subscribe(
+      recursos => {
+        this.dataSource.data = recursos;
+      },
+      error => {
+        console.log(error);
+      }
+    );
   }
 
-  openDialogAddRecurso(){
+  borrarRecurso(id: number) {
+    this.recursoService.deleteRecurso(id).subscribe(
+      response => {
+        this.getRecursos();
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  openDialogAddRecurso() {
     const dialogAddRecurso = this.dialog.open(AddRecursoComponent);
 
-    dialogAddRecurso.afterClosed().subscribe( response => {
-
-      this.getRecursos();
-    }, error => {
-      console.log(error);
-    }
-    
-    )
+    dialogAddRecurso.afterClosed().subscribe(
+      response => {
+        this.getRecursos();
+      },
+      error => {
+        console.log(error);
+      }
+    );
   }
-
 }
