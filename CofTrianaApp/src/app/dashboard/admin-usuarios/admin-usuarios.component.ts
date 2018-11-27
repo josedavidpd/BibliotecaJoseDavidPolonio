8077984e052fe964de77817892d3c40775ab7be4
jdpolonio <you@example.com>
2018-11-27 13:34:27 +0100

    Listar usuarios

--- CofTrianaApp/src/app/dashboard/admin-usuarios/admin-usuarios.component.ts
+++ CofTrianaApp/src/app/dashboard/admin-usuarios/admin-usuarios.component.ts
@@ -1,6 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { UsuariosService } from '../services/usuarios.service';
 import { Usuario } from '../interfaces/usuario.interface';
+import { MatPaginator, MatTableDataSource } from '@angular/material';
+
+
+const ELEMENT_DATA: Usuario[] = [];
+
+
+
 
 @Component({
   selector: 'app-admin-usuarios',
@@ -9,17 +16,40 @@ import { Usuario } from '../interfaces/usuario.interface';
 })
 export class AdminUsuariosComponent implements OnInit {
 
-  dataSource:Usuario[];
+  @ViewChild(MatPaginator)
+  paginator: MatPaginator;
+
+  displayedColumns: string[] = [
+    "Id",
+    "Nombre",
+    "Email",
+    "Telefonos",
+    "Notas",
+    "Rol",
+    "Acciones"
+  ];
+
 
   constructor(private usuarioService: UsuariosService) { }
 
   ngOnInit() {
+    this.getUsuarios();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 
   getUsuarios(){
     this.usuarioService.getAllUsuarios().subscribe(usuarios =>{
-      this.dataSource = usuarios;
+      this.dataSource.data = usuarios;
     }, error =>{
       console.log(error);
     })
