4ac83eb5b7eb2f75dec3f2a32c21611c7da0eee3
José David <38279195+josedavidpd@users.noreply.github.com>
2018-11-28 19:43:20 +0100

    Filtro y paginación en categorias

--- CofTrianaApp/src/app/dashboard/admin-categorias/admin-categorias.component.ts
+++ CofTrianaApp/src/app/dashboard/admin-categorias/admin-categorias.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CategoriasService } from '../services/categorias.service';
 import { Category } from '../interfaces/category.interface';
-import { AddCategoriaDto } from '../dto/add-categoria.dto';
 import { AddCategoriaComponent } from '../add-categoria/add-categoria.component';
-import { MatDialog } from '@angular/material';
+import { MatDialog, MatPaginator, MatTableDataSource } from '@angular/material';
 import { EditCategoriaComponent } from '../edit-categoria/edit-categoria.component';
+
+const ELEMENT_DATA: Category[] = [];
 
 @Component({
   selector: 'app-admin-categorias',
@@ -13,12 +14,16 @@ import { EditCategoriaComponent } from '../edit-categoria/edit-categoria.compone
 })
 export class AdminCategoriasComponent implements OnInit {
 
-  dataSource: Category[];
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+
   displayedColumns: string[] = [
     "Id",
     "Nombre",
     "Acciones"
   ];
+
+
 
   constructor(private dialog: MatDialog,private categoriaService: CategoriasService) { }
 
@@ -26,17 +31,30 @@ export class AdminCategoriasComponent implements OnInit {
     this.getAllCategorias();
   }
 
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+
+
 
   getAllCategorias(){
     this.categoriaService.getAllCategorias().subscribe(categorias =>{
-      this.dataSource = categorias;
+      this.dataSource.data = categorias;
     }, error =>{
       console.log(error);
     }
     )
   }
   openDialogAddCategoria(){
-    const dialogAddCategoria = this.dialog.open(AddCategoriaComponent);
+    const dialogAddCategoria = this.dialog.open(AddCategoriaComponent,{
+      height:'20%'
+    });
 
     dialogAddCategoria.afterClosed().subscribe(
       response => {
